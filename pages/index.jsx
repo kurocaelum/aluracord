9523251b35e97eabc70328bc8f778bd1889f9d13
Victor <victor_brandao@outlook.com>
2022-01-25 20:28:36 -0300

    State, novas páginas e navegação SPA vs a Tradicional

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 import appConfig from '../config.json'
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  )
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1'
@@ -50,24 +21,12 @@ function Titulo(props) {
   )
 }
 
-// function HomePage() {
-//   return (
-//     <div>
-//       <GlobalStyle />
-//       <Titulo tag='h1'>Can't escape from crossing fate!</Titulo>
-//       <h2>Discord - Alura Matrix</h2>
-//     </div>
-//   )
-// }
-
-// export default HomePage
-
 export default function PaginaInicial() {
-  const username = 'peas'
+  const [username, setUsername] = useState('kurocaelum')
+  const roteamento = useRouter()
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex',
@@ -101,6 +60,13 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as='form'
+            onSubmit={function(event) {
+              event.preventDefault()
+              console.log('Form submetido')
+              
+              roteamento.push('/chat')
+              // window.location.href = '/chat'
+            }}
             styleSheet={{
               display: 'flex',
               flexDirection: 'column',
@@ -116,8 +82,23 @@ export default function PaginaInicial() {
               {appConfig.name}
             </Text>
 
+            {/* <input 
+              type="text"
+              value={username}
+              onChange={function (event) { 
+                console.log('usuario digitou', event.target.value)
+                const valor = event.target.value
+                setUsername(valor)
+              }} /> */}
+
             <TextField
               fullWidth
+              value={username}
+              onChange={function (event) {
+                console.log('usuario digitou', event.target.value)
+                const valor = event.target.value
+                setUsername(valor)
+              }}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
