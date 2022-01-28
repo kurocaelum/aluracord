7d5fceb47eb1ceff06e6d50d6577090553492295
Victor <victor_brandao@outlook.com>
2022-01-28 09:53:14 -0300

    feat: integração com supabase (backend)

--- pages/chat.jsx
+++ pages/chat.jsx
@@ -1,31 +1,39 @@
 import { Box, Text, TextField, Image, Button } from '@skynexui/components'
+import { createClient } from '@supabase/supabase-js'
 import React from 'react'
 import appConfig from '../config.json'
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzM1NzMxNSwiZXhwIjoxOTU4OTMzMzE1fQ.XA7LVI8rEUVuxXiRGgjeOImwPYgagI93_AotAc4fS-M'
+const SUPABASE_URL = 'https://gatkvrftgzfkulgkwzng.supabase.co'
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 export default function ChatPage() {
 	const [mensagem, setMensagem] = React.useState('')
 	const [listaDeMensagens, setListaDeMensagens] = React.useState([])
 
-	/**
-	 * Usuário
-	 * - Usuário digita no campo textarea
-	 * - Enter para enviar
-	 * - Adicionar texto enviado na listagem
-	 *
-	 * Dev
-	 * - [X] Textarea criado
-	 * - [X] onChange com useState (if para limpar a variável caso seja enter)
-	 * - [] Liste de mensagens
-	 */
+	React.useEffect(() => {
+		supabaseClient
+			.from('mensagens')
+			.select('*')
+			.order('id', { ascending: false })
+			.then(({ data }) => {
+				setListaDeMensagens(data)
+			})
+	}, [])
 
 	function handleNovaMensagem(novaMensagem) {
 		const mensagem = {
-			id: listaDeMensagens.length + 1,
 			de: 'kurocaelum',
 			texto: novaMensagem,
 		}
 
-		setListaDeMensagens([mensagem, ...listaDeMensagens])
+		supabaseClient
+			.from('mensagens')
+			.insert([mensagem])
+			.then(({ data }) => {
+				setListaDeMensagens([data[0], ...listaDeMensagens])
+			})
+
 		setMensagem('')
 	}
 
@@ -162,7 +170,7 @@ function MessageList(props) {
 									display: 'inline-block',
 									marginRight: '8px',
 								}}
-								src={`https://github.com/kurocaelum.png`}
+								src={`https://github.com/${mensagem.de}.png`}
 							/>
 							<Text tag='strong'>{mensagem.de}</Text>
 							<Text
